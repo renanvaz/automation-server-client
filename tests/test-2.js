85b5c5c83d4299a02decb5fdfb1aae1573462566
Renan Vaz <renan.c.vaz@gmail.com>
2015-07-20 01:58:28 -0300

    Working...

--- tests/test-2.js
+++ tests/test-2.js
@@ -3,9 +3,10 @@
 var Helpers = {
     exec: require('child_process').exec,
     execSync: require('child_process').execSync,
+    spawnSync: require('child_process').spawnSync,
     sleep: function(time) {
         this.execSync('sleep '+time);
     }
 };
 
-Helpers.execSync('sudo bash', ['blink.sh']);
+Helpers.spawnSync('sudo sh', ['blink.sh']);
