7bb62804a521e0cca49998a19e5af967858e45f6
Renan Vaz <renan.c.vaz@gmail.com>
2015-07-20 01:49:30 -0300

    Working...

--- tests/blink.sh
+++ tests/blink.sh
@@ -3,7 +3,7 @@
 if [ ! -d "/sys/class/gpio/gpio17" ]; then echo "17" > /sys/class/gpio/export; fi
 echo "out" > /sys/class/gpio/gpio17/direction
 
-for ( i=1; i <= 20; i++ )
+for (( i=1; i <= 20; i++ ))
 do
   echo "1" > /sys/class/gpio/gpio17/value
   sleep .01
