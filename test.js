df765fd52333eec4b18485f33b31ba1eeedaf322
Renan Vaz <renan.c.vaz@gmail.com>
2015-07-20 00:57:36 -0300

    Working...

--- test.js
+++ test.js
@@ -57,7 +57,7 @@ var LATCH_PIN = GPIO.setup('17', GPIO.OUT);
 
 for (var i = 0; i < 10; i++) {
     LATCH_PIN.out(1);
-    Helpers.sleep(.2);
+    //Helpers.sleep(.2);
     LATCH_PIN.out(0);
-    Helpers.sleep(.2);
+    //Helpers.sleep(.2);
 }
