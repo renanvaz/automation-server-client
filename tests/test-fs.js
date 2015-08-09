4d8af566ac621c1c8085baab6021b345926e953f
Renan Vaz <renan.c.vaz@gmail.com>
2015-08-09 01:16:28 -0300

    Working...

--- tests/test-fs.js
+++ tests/test-fs.js
@@ -60,12 +60,15 @@ var DATA_PIN = GPIO.setup('17', GPIO.OUT);
 var CLOCK_PIN = GPIO.setup('22', GPIO.OUT);
 var LATCH_PIN = GPIO.setup('27', GPIO.OUT);
 
-var data = [0,0,1,0,1,1,0,0,0,1,0,0,1,0,1,1, 0,0,1,0,1,1,0,0,0,1,0,0,1,0,1,1, 0,0,1,0,1,1,0,0,0,1,0,0,1,0,1,1, 0,0,1,0,1,1,0,0,0,1,0,0,1,0,1,1].reverse();
+var data = [];
+for (var i = 0; i < 64; i++) {
+    data.push(Math.round(Math.random()));
+}
 
 var timeStart = new Date().getTime();
 
 LATCH_PIN.out(0);
-for (var i = 0; i < data.length; i++) {
+for (i = 0; i < data.length; i++) {
     CLOCK_PIN.out(0);
     DATA_PIN.out(data[i]);
     CLOCK_PIN.out(1);
