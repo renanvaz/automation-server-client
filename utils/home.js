beb20b9ed9813f932ce48a62d2666ded441b4cd7
Renan Vaz <renan.c.vaz@gmail.com>
2016-01-04 17:35:53 -0200

    Working...

--- utils/home.js
+++ utils/home.js
@@ -48,11 +48,10 @@ function Home (serverURL) {
 Home.prototype.hardware = function() {
     var counter = 0;
 
-    this.gpio.LATCH.out(0);
     this.gpio.CLEAR.out(0); // Clear in 0
     this.gpio.CLEAR.out(1); // Back to normal
-    this.gpio.LATCH.out(1);
 
+    this.gpio.LATCH.out(0);
     do {
         counter++;
 
@@ -60,11 +59,10 @@ Home.prototype.hardware = function() {
         this.gpio.DATA.out(1);
         this.gpio.CLOCK.out(1);
     } while (this.gpio.END.in() != 1);
-
-    this.gpio.LATCH.out(0);
-    this.gpio.CLEAR.out(0); // Clear in 0
-    this.gpio.CLEAR.out(1); // Back to normal
     this.gpio.LATCH.out(1);
+
+    // this.gpio.CLEAR.out(0); // Clear in 0
+    // this.gpio.CLEAR.out(1); // Back to normal
 
     Helpers.write(CONFIG_FILE, JSON.stringify({devices: counter}));
 
