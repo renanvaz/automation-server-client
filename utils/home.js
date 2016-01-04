44b80dc781a1d5bc7690144579c0c4c44e4050d5
Renan Vaz <renan.c.vaz@gmail.com>
2016-01-04 17:34:40 -0200

    Working...

--- utils/home.js
+++ utils/home.js
@@ -48,8 +48,10 @@ function Home (serverURL) {
 Home.prototype.hardware = function() {
     var counter = 0;
 
+    this.gpio.LATCH.out(0);
     this.gpio.CLEAR.out(0); // Clear in 0
     this.gpio.CLEAR.out(1); // Back to normal
+    this.gpio.LATCH.out(1);
 
     do {
         counter++;
@@ -59,8 +61,10 @@ Home.prototype.hardware = function() {
         this.gpio.CLOCK.out(1);
     } while (this.gpio.END.in() != 1);
 
+    this.gpio.LATCH.out(0);
     this.gpio.CLEAR.out(0); // Clear in 0
     this.gpio.CLEAR.out(1); // Back to normal
+    this.gpio.LATCH.out(1);
 
     Helpers.write(CONFIG_FILE, JSON.stringify({devices: counter}));
 
--- utils/messages.js
+++ utils/messages.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     error: {
-        noConfigFile: 'O arquivo de configuração "homes.json" não foi encontrado.'
+        noConfigFile: 'O arquivo de configuração "homez.json" não foi encontrado.'
     }
 };
